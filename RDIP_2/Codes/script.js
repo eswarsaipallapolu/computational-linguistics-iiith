12d4ecdddf53270d2f7f6d1d8363164436249e0e
eswarsaipallapolu <eswarsai8237@gmail.com>
2020-07-21 23:17:08 +0530

    Unit-5 goal-5

--- RDIP_2/Codes/script.js
+++ RDIP_2/Codes/script.js
@@ -1,6 +1,6 @@
 var ENGLISH = ['The child liked the chocolate','She was stopped by the bravest knight','Mary baked a cake for his birthday','She decorated the cake carefully','Mary wore a dress with polka dots'];
 var HINDI = ['राम ने सीता के लिए फल तोड़ा।', 'छोटे बच्चे पाठशाला जल्दी आयेंगे।', 'मेहनत का फल मीठा होता है।', 'वाह! वह खूबसूरत है।', 'पेड़ से पत्ते गिर गए।'];
-var sentences = "";
+var sentences = "",postdrop;
 
 function selection() {
     if (document.getElementById('default').selected)
@@ -39,6 +39,8 @@ function disk() {
             sentence = ENGLISH[3];
         else if (document.getElementById('e5').selected)
             sentence = ENGLISH[4];
+        sentence = sentence.replace(".","");
+        postdrop = "<option id='pos1' value='Noun'>Noun</option><option id='pos2' value='Pronoun'>Pronoun</option><option id='pos3' value='Verb'>Verb</option><option id='pos4' value='Adjective'>Adjective</option><option id='pos5' value='Adverb'>Adverb</option><option id='pos6' value='Determiner'>Determiner</option><option id='pos7' value='Preposition'>Preposition</option><option id='pos8' value='Conjunction'>Conjunction</option><option id='pos9' value='Interjection'>Interjection</option>";
     }
     else if (document.getElementById('hindi').selected) {
         if (document.getElementById('e1').selected)
@@ -51,11 +53,13 @@ function disk() {
             sentence = HINDI[3];
         else if (document.getElementById('e5').selected)
             sentence = HINDI[4];
+        sentence = sentence.replace("।","");
+        postdrop = "<option id='pos1' value='Noun'>Noun</option><option id='pos2' value='Pronoun'>Pronoun</option><option id='pos3' value='Verb'>Verb</option><option id='pos4' value='Adjective'>Adjective</option><option id='pos5' value='Adverb'>Adverb</option><option id='pos6' value='Postposition'>Postposition</option><option id='pos7' value='Conjunction'>Conjunction</option><option id='pos8' value='Interjection'>Interjection</option>";
     }
     sentence = sentence.split(" ");
     var column = "<tr id='rowhead' style='color:brown'><td>LEXICON</td><td>POS</td><td></td><td></td></tr>";
     for (var j = 0; j < sentence.length; j++)
-        column = column + "<tr id='id" + j + "'><td>" + sentence[j] + "</td><td></td><td></td><td></td></tr>";
+        column = column + "<tr id='id" + j + "'><td>" + sentence[j] + "</td><td><select id='postdropdown" + j + "' class='postdropdown'>" + postdrop + "</select></td><td></td><td></td></tr>";
     document.getElementById('post').innerHTML = column.trim();
 
 }
